114ede7ac185acb80ea5541363e0eb7edf081989
Lucas Benedicic <lucas.benedicic@gmail.com>
2011-11-05 17:58:14 +0100

    Avoid browsing of the public site using a superuser, since many things break.-

--- views.py
+++ views.py
@@ -10,11 +10,14 @@ def home (request, template_name='welcome.html'):
     Displays the homepage of this site, depending on the user.-
     """
     if request.user.is_authenticated ( ):
-        user = UserProfile.objects.get_profile (request.user.username)
-        if user.is_player ( ):
-            template_name = 'players/home.html'
-        elif user.is_club ( ):
-            template_name = 'clubs/home.html'
+        if request.user.is_superuser:
+            template_name = 'superuser_not_allowed.html'
+        else:
+            user = UserProfile.objects.get_profile (request.user.username)
+            if user.is_player ( ):
+                template_name = 'players/home.html'
+            elif user.is_club ( ):
+                template_name = 'clubs/home.html'
         
     return render_to_response (template_name,
                                context_instance=RequestContext(request))
